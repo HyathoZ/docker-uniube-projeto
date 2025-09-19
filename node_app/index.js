51edb638b01c4a84d4245534b75ef1f0fbdd3676
Joao Santos <joao0188@[D[D[D[D[D_[C[C[C[C[outlook.com>
2025-09-19 16:38:40 -0300

    hotreload

--- node_app/index.js
+++ node_app/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
-
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+
 // Configuração do MySQL (igual ao docker-compose)
 const dbConfig = {
-  host: "mysql",       // nome do serviço no docker-compose
+  host: "mysql", // nome do serviço no docker-compose
   user: "appuser",
   password: "apppass",
-  database: "appdb"
+  database: "appdb",
 };
 
 app.get("/", (req, res) => {
@@ -21,7 +22,10 @@ app.get("/api/v1/cliente/:id", async (req, res) => {
   try {
     const cliente = req.params.id;
     const connection = await mysql.createConnection(dbConfig);
-    const [rows] = await connection.execute("SELECT * FROM clientes WHERE id = ?", [cliente]);
+    const [rows] = await connection.execute(
+      "SELECT * FROM clientes WHERE id = ?",
+      [cliente]
+    );
     await connection.end();
     res.json(rows);
   } catch (err) {
@@ -29,6 +33,57 @@ app.get("/api/v1/cliente/:id", async (req, res) => {
   }
 });
 
+app.post("/api/v1/cliente", async (req, res) => {
+  try {
+    const { nome, email, telefone } = req.body;
+    if (!nome || !email || !telefone) {
+      return res
+        .status(400)
+        .json({ error: "Campos obrigatórios: nome, email, telefone" });
+    }
+    const connection = await mysql.createConnection(dbConfig);
+    const [result] = await connection.execute(
+      "INSERT INTO clientes (nome, email, telefone) VALUES (?, ?, ?)",
+      [nome, email, telefone]
+    );
+    await connection.end();
+    res.status(201).json({
+      message: "Cliente criado com sucesso!",
+      clienteId: result.insertId,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.delete("/api/v1/cliente/:id", async (req, res) => {
+  try {
+    const cliente = req.params.id;
+    const connection = await mysql.createConnection(dbConfig);
+    await connection.execute("DELETE FROM vendas WHERE cliente_id = ?", [
+      cliente,
+    ]);
+    const [rows] = await connection.execute(
+      "DELETE FROM clientes WHERE id = ?",
+      [cliente]
+    );
+    await connection.end();
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.put("/api/v1/cliente/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nome, email, telefone } = req.body;
+    const connection = await mysql.createConnection(dbConfig);
+    await connection.execute(`UPDATE clientes SET nome = ?, email = ?, telefone = ? WHERE id = ?`[nome, email, telefone, id]);
+    res.send(`Cliente ${nome} atualizado com sucesso!`);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 app.listen(PORT, () => {
   console.log(`🚀 Servidor Node rodando na porta ${PORT}`);
 });
